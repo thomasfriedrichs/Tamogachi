31475bccf96a71f2d3df2f800e258b87f256cf0d
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-12 11:25:15 -0700

    add business logic
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

---
+++ Tamogachi/Controllers/Tamogachi.Controller.cs
@@ -0,0 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
+using Tamogachi.Models;
+using System.Collections.Generic;
+
--- Tamogachi/Models/Tamogachi.cs
+++ Tamogachi/Models/Tamogachi.cs
@@ -1,11 +1,64 @@
 using System;
+using System.Collections.Generic;
 
 namespace Tamogachi.Model 
 {
   class Tamogachi
   {
     public int Hunger { get; set; }
-    public int Food { get; set; }
-    
-  }
+    public int Play { get; set; }
+    public int Sleep { get; set; }
+    public int Id { get; }
+    private static List<Tamogachi> tamogachis = new List<Tamogachi> { };
+
+    public static void Tamogachi (int hunger, int play, int sleep)
+    {
+      Hunger = hunger;
+      Play = play;
+      Sleep = sleep;
+      tamogachis.Add(this);
+      Id = tamogachis.Count;
+    }
+    // Find method
+    public static Tamogachi Find(int searchId)
+    {
+      return tamogachis[searchId - 1];
+    }
+
+    // GetAll method
+    public static void GetAll()
+    {
+      return tamogachis;
+    }
+
+    // ClearAll method
+    public static void ClearAll()
+    {
+      tamogachis.Clear();
+    }
+
+    // Clear Tamogachi instance?
+    public static void Death(int tamogachiId)
+    {
+      tamogachis.RemoveAt(tamogachiId);
+    }
+
+    // Feed method?
+    public static void Feed()
+    {
+      Tamogachi.Hunger += 10;
+    }
+
+    // PLay method
+    public static void AddPlay()
+    {
+      Tamogachi.Play += 10;
+    }
+
+    // sleep method 
+    public static void Rest()
+    {
+      Tamogachi.Sleep += 10;
+    }
+  } 
 }
